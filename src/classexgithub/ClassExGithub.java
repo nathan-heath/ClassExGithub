bc51f299348ce6b872c0d3ed721a1d4e76090a99
nsh4@students.ptcollege.edu <>
2019-02-01 09:58:51 -0500

    This is the first commit

---
+++ src/classexgithub/ClassExGithub.java
@@ -0,0 +1,12 @@
+package classexgithub;
+
+public class ClassExGithub {
+
+    public static void main(String[] args) {
+    Heath();
+    }
+    
+    public static void Heath(){
+        System.out.println("Nathan Heath!!");
+    }
+}
